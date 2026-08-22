74f7c14b1539419a469f98b4a53dd9bb8632fef5
Gianluca Pettenon <gianpettenon@gmail.com>
2026-08-22 19:28:24 +0200

    fix: database schema

--- migrations/000001_create_urls.up.sql
+++ migrations/000001_create_urls.up.sql
@@ -1,8 +1,5 @@
 CREATE TABLE urls (
-    id BIGSERIAL PRIMARY KEY,
-    short_code VARCHAR(12) NOT NULL,
+    id BIGINT PRIMARY KEY,
     original_url TEXT NOT NULL,
     created_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
 );
-
-CREATE UNIQUE INDEX idx_urls_short_code ON urls(short_code);
